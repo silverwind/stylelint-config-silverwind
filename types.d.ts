c4999fefc83490cdd26ce1cc275daeb0cabe83cc
silverwind <me@silverwind.io>
2025-11-08 14:38:25 +0100

    type stubs

---
+++ types.d.ts
@@ -0,0 +1,15 @@
+declare module "stylelint-declaration-block-no-ignored-properties" {
+  import type {Plugin} from "stylelint";
+  const plugin: Plugin;
+  export = plugin;
+}
+declare module "stylelint-value-no-unknown-custom-properties" {
+  import type {Plugin} from "stylelint";
+  const plugin: Plugin;
+  export = plugin;
+}
+declare module "@stylistic/stylelint-plugin" {
+  import type {Plugin} from "stylelint";
+  const plugin: Plugin;
+  export = plugin;
+}
